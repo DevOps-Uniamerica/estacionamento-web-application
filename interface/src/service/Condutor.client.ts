b4f118dbf39419af38700dbd42ef5354ea2bbff4
André Mai Velasco <102202764+TheAndrew1@users.noreply.github.com>
2025-03-11 19:13:57 -0300

    Update Condutor.client.ts

--- interface/src/service/Condutor.client.ts
+++ interface/src/service/Condutor.client.ts
@@ -6,7 +6,7 @@ export class CondutorClient {
 
   constructor() {
     this.axiosClient = axios.create({
-      baseURL: `http://localhost:8080/api`,
+      baseURL: `http://host.docker.internal:8080/api`,
       headers: { 'Content-type': 'application/json' }
     });
   }
