50b13483a5e1c8c31079b286dec660afc1ffb18a
André Mai Velasco <102202764+TheAndrew1@users.noreply.github.com>
2025-06-03 20:18:55 -0300

    Update deplay.tf

--- ansible/deplay.tf
+++ ansible/deplay.tf
@@ -3,23 +3,6 @@ provider "google" {
   region  = "southamerica-east1"
 }
 
-resource "tls_private_key" "vm_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "private_key" {
-  content         = tls_private_key.vm_ssh_key.private_key_openssh
-  filename        = "${path.module}/.ssh/gcp-vm-key.pem"
-  file_permission = "0600" 
-}
-
-
-resource "local_file" "public_key" {
-  content  = tls_private_key.vm_ssh_key.public_key_openssh
-  filename = "${path.module}/.ssh/gcp-vm-key.pub"
-}
-
 resource "google_compute_instance" "vm" {
   name         = "vm-aplicacao"
   machine_type = "e2-micro"
@@ -37,7 +20,7 @@ resource "google_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${tls_private_key.vm_ssh_key.public_key_openssh}"
+    ssh-keys = "${{ secrets.GCP_USER }}:${{ secrets.SSH_KEY_PUB }}"
   }
 
   tags = ["ssh", "http-server"]
