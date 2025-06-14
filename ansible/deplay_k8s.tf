c4426d3a6ceb3037131c0297d220136a08a9fb99
AndreiBack <107830134+AndreiBack@users.noreply.github.com>
2025-06-13 22:24:58 -0300

    Update deplay_k8s.tf

--- ansible/deplay_k8s.tf
+++ ansible/deplay_k8s.tf
@@ -1,76 +1,50 @@
-
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
-    google = { source = "hashicorp/google" version = "~> 4.0" }
-    google-beta = { source = "hashicorp/google-beta" version = "~> 4.0" }
-  }
-}
-
-# Variáveis obrigatórias
-variable "project" {
-  description = "GCP project ID"
-  type        = string
-}
-variable "region" {
-  description = "GCP region"
-  type        = string
-}
-variable "zone" {
-  description = "GCP zone"
-  type        = string
-}
-variable "gcp_user" {
-  description = "Usuário SSH para VM"
-  type        = string
-}
-variable "ssh_key_pub" {
-  description = "Chave pública SSH para acesso à VM"
-  type        = string
-}
-
-provider "google" {
-  project = var.project
-  region  = var.region
-}
-
-resource "google_compute_instance" "vm" {
-  name         = "vm-aplicacao"
-  machine_type = "e2-micro"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 10
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
     }
   }
 
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
-
-  metadata = {
-    ssh-keys = "${var.gcp_user}:${var.ssh_key_pub}"
-  }
-
-  tags = ["ssh", "http-server"]
+  backend "gcs" {}
 }
 
-resource "google_compute_firewall" "firewall" {
-  name    = "allow-ssh-and-app"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "8080"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
-output "vm_ip_address" {
-  description = "IP público da instância"
-  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+provider "google-beta" {
+  alias   = "beta"
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  networking_mode          = "VPC_NATIVE"
+  ip_allocation_policy     {}
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.cluster_name}-np"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
