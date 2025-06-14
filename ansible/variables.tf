804cd26180a5fa3695d3b6d423235d441777b4e4
AndreiBack <107830134+AndreiBack@users.noreply.github.com>
2025-06-14 02:19:55 -0300

    Update variables.tf

--- ansible/variables.tf
+++ ansible/variables.tf
@@ -1,31 +1,9 @@
-// variables.tf
 variable "gcp_user" {
-  type        = string
+  type = string
   description = "Usuário SSH"
 }
 
 variable "ssh_key_pub" {
-  type        = string
+  type = string
   description = "Chave pública SSH"
-}
-
-variable "project_id" {
-  type        = string
-  description = "ID do projeto GCP"
-}
-
-variable "region" {
-  type        = string
-  description = "Região onde criar recursos (ex: southamerica-east1)"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "Nome do cluster GKE"
-}
-
-variable "node_count" {
-  type        = number
-  description = "Número inicial de nós no node pool"
-  default     = 1
 }
